75f3a54eb4ad0634b374df4158c0690f49fac8b5
Ruy <ruymiguel.penha2003@gmail.com>
2021-05-06 17:08:01 -0400

    created first commit

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var dadoImageView1: UIImageView!
+    @IBOutlet weak var dadoImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        dadoImageView1.image = #imageLiteral(resourceName: "DiceOne")
+        dadoImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        
+        dadoImageView1.alpha = 0.5
+
     }
 
 
