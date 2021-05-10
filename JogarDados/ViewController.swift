68b3dfb18698cf1443fea36d52e333a4eb3cb917
Adriano Grevizirsky <69660147+grevizirsky@users.noreply.github.com>
2021-05-10 17:37:16 -0400

    aula dia 10/05

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //MARK: - Outlets
+    @IBOutlet weak var dadoImageView1: UIImageView!
+    @IBOutlet weak var dadoImageView2: UIImageView!
+    
+    //MARK: - Conection
+    @IBAction func rolarDadosPressed(_ sender: UIButton) {
+        let arrayDados = [UIImage(named: "DiceOne"),
+                          UIImage(named: "DiceTwo"),
+                          UIImage(named: "DiceThree"),
+                          UIImage(named: "DiceFour"),
+                          UIImage(named: "DiceFive"),
+                          UIImage(named: "DiceSix")]
+        
+        dadoImageView1.image = arrayDados[Int.random (in: 1...5)]
+        dadoImageView2.image = arrayDados[Int.random (in: 1...5)]
+        
+        }
+    
     }
 
-
-}
 
